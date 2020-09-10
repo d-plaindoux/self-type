08ca1e2569b26380ff3e23dd416bc20084314c2f
d-plaindoux <d.plaindoux@free.fr>
2020-09-10 08:39:44 +0200

    Correct typos in the presentations

--- examples/ocaml/PointOCaml.ml
+++ examples/ocaml/PointOCaml.ml
@@ -2,12 +2,12 @@ class point (x:int) (y:int) =
     object (self : 'self)
         val x = x
         val y = y
-        method get_x = x
-        method get_y = y
+        method x = x
+        method y = y
 
         method init x y = {< x = x ; y = y >}
         method move dx dy = self#init (x + dx) (y + dy)
-        method equal (p:'self) = x = p#get_x && y = p#get_y
+        method equal (p:'self) = x = p#x && y = p#y
     end;;
 
 class point_colore (x:int) (y:int) (c:int) =
@@ -15,8 +15,8 @@ class point_colore (x:int) (y:int) (c:int) =
         inherit point x y as super
 
         val c = c
-        method get_c = c
+        method c = c
 
         method init x y = {< x = x ; y = y; c = c >}
-        method equal (p:'self) = c = p#get_c && super#equal p
+        method equal (p:'self) = c = p#c && super#equal p
     end;;
